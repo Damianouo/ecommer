c682b2c7702e77fc9649f4b21b0366962ad2a57c
Damian <Damian@fakemail.com>
2023-03-08 23:31:30 +0800

    doning responsive, 2:43

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,18 @@
 import { Search, ShoppingCartOutlined } from "@mui/icons-material";
 import Badge from "@mui/material/Badge";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 60px;
+  ${mobile({ height: "50px" })}
 `;
 const Wrapper = styled.div`
   padding: 20px 40px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  ${mobile({ padding: "10px 0" })}
 `;
 
 //'Left side
@@ -22,6 +25,7 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({ display: "none" })}
 `;
 
 const SearchContainer = styled.div`
@@ -34,6 +38,7 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
   border: none;
+  ${mobile({ width: "50px" })}
 `;
 
 //'Center side
@@ -45,6 +50,7 @@ const Center = styled.div`
 const Logo = styled.h1`
   font-size: 40px;
   font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
 `;
 
 const Right = styled.div`
@@ -52,12 +58,15 @@ const Right = styled.div`
   display: flex;
   justify-content: flex-end;
   align-items: center;
+
+  ${mobile({ justifyContent: "center" })}
 `;
 
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })};
 `;
 
 const Navbar = () => {
@@ -67,7 +76,7 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input />
+            <Input placeholder='search' />
             <Search style={{ color: "gray", fontSize: "16px" }} />
           </SearchContainer>
         </Left>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,8 @@
-import styled from 'styled-components';
-import Annoucement from '../components/Annoucement';
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
+import { Add, Remove } from "@mui/icons-material";
+import styled from "styled-components";
+import Annoucement from "../components/Annoucement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 const Container = styled.div``;
 
@@ -18,6 +19,7 @@ const Top = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  padding: 20px;
 `;
 
 const TopButtom = styled.button`
@@ -25,10 +27,10 @@ const TopButtom = styled.button`
   font-weight: 600;
   cursor: pointer;
 
-  border: ${(props) => props.type === 'filled' && 'none'};
+  border: ${(props) => props.type === "filled" && "none"};
   background-color: ${(props) =>
-    props.type === 'filled' ? 'black' : 'transparant'};
-  color: ${(props) => props.type === 'filled' && 'white'};
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
 `;
 
 const TopTexts = styled.div``;
@@ -38,7 +40,109 @@ const TopText = styled.span`
   margin: 0 10px;
 `;
 
-const Botton = styled.div``;
+const Botton = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0;
+`;
+const Info = styled.div`
+  flex: 3 1 75%;
+`;
+
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 0;
+`;
+const ProductDetail = styled.div`
+  flex: 2 1 66%;
+  display: flex;
+`;
+const Image = styled.img`
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+`;
+const Details = styled.div`
+  padding: 0 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+const ProductName = styled.span``;
+
+const ProductID = styled.span``;
+
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.bg};
+`;
+const ProductSize = styled.span``;
+
+const PriceDetail = styled.div`
+  flex: 1 1 33%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 40px;
+`;
+
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 10px;
+`;
+
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+`;
+
+const Hr = styled.hr`
+  background-color: #c0c0c0;
+  border: none;
+  height: 1px;
+`;
+
+const Summary = styled.div`
+  flex: 1 1 25%;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
+`;
+
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+
+const SummaryItem = styled.div`
+  margin: ${(props) => (props.type === "total" ? "50px 0" : "30px 0")};
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+
+const SummaryItemText = styled.span``;
+
+const SummaryItemPrice = styled.span``;
+
+const SummaryButton = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+  font-size: 20px;
+`;
 
 const Cart = () => {
   return (
@@ -53,10 +157,84 @@ const Cart = () => {
             <TopText>Shopping Bag(2)</TopText>
             <TopText>Your Wishlist(2)</TopText>
           </TopTexts>
-          <TopButtom type="filled">CHECK NOW</TopButtom>
+          <TopButtom type='filled'>CHECKOUT NOW</TopButtom>
         </Top>
 
-        <Botton></Botton>
+        <Botton>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src='https://source.unsplash.com/BeZGTKfVEOs' />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> JESSIE THUNDER SHOES
+                  </ProductName>
+                  <ProductID>
+                    <b>ID:</b> 93813718293
+                  </ProductID>
+                  <ProductColor bg='black' />
+                  <ProductSize>
+                    <b>Size:</b> 37.5
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 30</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetail>
+                <Image src='https://source.unsplash.com/RqYTuWkTdEs' />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> HAKUYO SHIRT
+                  </ProductName>
+                  <ProductID>
+                    <b>ID:</b> 93813189396
+                  </ProductID>
+                  <ProductColor bg='black' />
+                  <ProductSize>
+                    <b>Size:</b> M
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>1</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 20</ProductPrice>
+              </PriceDetail>
+            </Product>
+          </Info>
+          <Summary>
+            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type='total'>
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryButton>CHECKOUT NOW</SummaryButton>
+          </Summary>
+        </Botton>
       </Wrapper>
       <Footer />
     </Container>
